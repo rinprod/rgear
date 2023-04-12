7b216f10ba8751309cd813f489d7d645cba0bf2e
Mark Sellors <github@5vcc.com>
2023-04-12 18:02:30 +0100

    improves code, maintainability and docs

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # rgear - Generate R helper files
 
-The aim of `rgear` is to provide simple tooling to create the necessary helper
-files used by Domino Data Lab when serving R based content.
+The aim of `rgear` is to provide simple tooling to create helper files for R based content.
+
+The files it creates allow you to serve R based content (Shiny apps, plumber APIs or RMarkdown html outputs) from the command line.
+
+Originally created for use with Domino Data Lab, `rgear` can be used wherever R is and therefore also makes a great complement to other projects such as (R4Pi)[https://r4pi.org].
 
 Supported content types:
 
 * Shiny apps
 * Rmarkdown html output
 * Plumber APIs
+
 
 ## Installation
 
@@ -17,6 +21,7 @@ Supported content types:
 # python3 -m pip install rgear
 ```
 
+
 ## Usage
 
 ```bash
@@ -24,28 +29,75 @@ rgear --help
 ```
 
 ```output
-usage: rgear [-h] [-f] [-v] [--version] {shiny,plumber,rmarkdown} path
+usage: rgear [-h] [-f] [-p PORT] [-v] [--version]
+             {shiny,plumber,rmarkdown} path
 
-Generates R helpers for Domino
+Generates helper scripts for starting R based content servers
 
 positional arguments:
   {shiny,plumber,rmarkdown}
-                        Choose the application type you want to serve
+                        choose the application type you want to serve
   path                  path to the content to serve
 
 optional arguments:
   -h, --help            show this help message and exit
-  -f, --force           Forces overwrite if the output file(s) already exist
-  -v, --verbose
-  --version             show program's version number and exit
+  -f, --force           force overwrite if the output file(s) already exist
+  -p PORT, --port PORT  override the default port setting. [default: 8888]
+  -v, --verbose         enable verbose output
+  --version             print the version and exit
 
 For detailed help, please see https://github.com/sellorm/rgear
 ```
 
-Using the above options, various files -- mostly `app.sh` -- will be written to
-the working directory with the required parameters already populated.
+Using the above options, various files -- mostly `app.sh` -- will be written to the working directory with the required parameters already populated.
 
-Domino Data Lab can then use these files to serve your content as a DDL "app".
+The `app.sh` file created by rgear can be used to start the specified content from the command line. This makes it an ideal complement to all command-line uses of R.
+
+For example, to generate a script to serve a plumber API:
+
+```bash
+# rgear plumber plumber.R
+```
+
+
+## Using with Domino Data Lab
+
+Domino Data Lab can then use these files to serve your content as a DDL
+"(app)[https://docs.dominodatalab.com/en/latest/user_guide/71635d/publish-apps/]".
+
+The "app.sh" files created by `rgear` have sensible defaults for Domino and allows users to generate the file(s) they need without too much effort.
+
+Use `rgear` to generate the "app.sh" file you need for your specific content type and then configure Domino to use that file to serve your content.
+
+
+## Using with command line R (Including R4Pi)
+
+Use `rgear` to generate startup scripts for your Shiny apps, plumber APIs or RMarkdown reports.
+
+For example, if you have a Shiny app in an "app.R" file, run:
+
+```bash
+# rgear shiny app.R
+```
+
+Then you can run the app with:
+
+```bash
+# ./app.sh
+```
+
+Then visit the app in your browser using either http://localhost:8888 or http://<IP ADDRESS>:8888.
+
+Use ctrl+c to stop the app from running.
+
+You can run the "app.sh" script in the background with:
+
+```bash
+# nohup ./app.sh &
+```
+
+You'll need to use `ps` and `kill` to stop an application started this way.
+
 
 ## License
 
--- rgear/__init__.py
+++ rgear/__init__.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
 """
-Command line utility to generate various types of helper script for using R in Domino
+Command line utility to generate various types of helper script for starting
+R based servers
 """
 
 import argparse
 import os.path
+from os import chmod
 import sys
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 # TEMPLATES
 
 SHINY_APP_START = """\
-# Simple shiny app startup script for use on Domino
+# Shiny app startup script
 # Generated with rgear (https://github.com/sellorm/rgear)
-R -e 'shiny::runApp("{}", host="0.0.0.0", port=8888)'
+R -e 'shiny::runApp("{}", host="0.0.0.0", port={})'
 """
 
 PLUMBER_APP_START = """\
-# Simple plumber API startup scriptt for use on Domino
+# Plumber API startup script
 # Generated with rgear (https://github.com/sellorm/rgear)
-R -e 'plumber::pr_run(plumber::pr("{}"), host = "0.0.0.0", port = 8888)'
+R -e 'plumber::pr_run(plumber::pr("{}"), host="0.0.0.0", port={})'
 """
 
 RMARKDOWN_APP_START = """\
-# quick web server for serving an Rmarkdown docuent for use on Domino
+# Web server for serving an Rmarkdown document
 # Generated with rgear (https://github.com/sellorm/rgear)
-python3 -m http.server --bind "0.0.0.0" 8888
+python3 -m http.server --bind "0.0.0.0" {}
 """
 
 RMARKDOWN_APP_INDEX = """\
@@ -87,31 +89,34 @@ def file_does_not_exist(file, print_err=True):
 # File generator functions
 
 
-def generate_shiny(shiny_app, overwrite=False, verbose=False):
+def generate_shiny(shiny_app, overwrite=False, verbose=False, port=8888):
     """
-    Generates helper file for serving shiny apps
+    Generate helper files for serving shiny apps
 
     Keyword Arguments:
     shiny_app      -- path to your shiny app
     overwrite      -- whether to overwrite existing helper files
     verbose        -- prints more verbose output
+    port           -- The port to listen on [default: 8888]
 
     Generated file:
-    * app.sh -- starts the shiny app with Domino specific parameters
+    * app.sh -- starts the shiny app
     """
     print("Generating app.sh...")
     if not overwrite:
         file_exists("app.sh", print_err=True)
     file_does_not_exist(shiny_app, print_err=True)
+    app_template = SHINY_APP_START.format(shiny_app, port)
     with open("app.sh", "w", encoding="utf8") as file:
-        file.write(SHINY_APP_START.format(shiny_app))
+        file.write(app_template)
+    chmod("app.sh", 0o755)
     if verbose:
         print("---- app.sh ----")
-        print(SHINY_APP_START.format(shiny_app))
+        print(app_template)
     print("Complete.")
 
 
-def generate_rmarkdown(rmarkdown_html, overwrite=False, verbose=False):
+def generate_rmarkdown(rmarkdown_html, overwrite=False, verbose=False, port=8888):
     """
     Generates helper files for serving Rmarkdown documents
 
@@ -119,6 +124,7 @@ def generate_rmarkdown(rmarkdown_html, overwrite=False, verbose=False):
     rmarkdown_html -- path to your Rmarkdown file's html output
     overwrite      -- whether to overwrite existing helper files
     verbose        -- prints more verbose output
+    port           -- The port to listen on [default: 8888]
 
     Generated files:
     * app.sh     -- starts a webserver to serve the generated Rmarkdown html
@@ -130,20 +136,23 @@ def generate_rmarkdown(rmarkdown_html, overwrite=False, verbose=False):
     if not overwrite:
         file_exists("index.html", print_err=True)
     file_does_not_exist(rmarkdown_html, print_err=True)
+    app_template = RMARKDOWN_APP_START.format(port)
+    index_template = RMARKDOWN_APP_INDEX.format(rmarkdown_html)
     with open("app.sh", "w", encoding="utf8") as file:
-        file.write(RMARKDOWN_APP_START)
+        file.write(app_template)
+    chmod("app.sh", 0o755)
     if verbose:
         print("---- app.sh ----")
-        print(RMARKDOWN_APP_START)
+        print(app_template)
     with open("index.html", "w", encoding="utf8") as file:
-        file.write(RMARKDOWN_APP_INDEX.format(rmarkdown_html))
+        file.write(index_template)
     if verbose:
-        print("---- app.sh ----")
-        print(RMARKDOWN_APP_INDEX.format(rmarkdown_html))
+        print("---- index.html ----")
+        print(index_template)
     print("Complete.")
 
 
-def generate_plumber(plumber_file, overwrite=False, verbose=False):
+def generate_plumber(plumber_file, overwrite=False, verbose=False, port=8888):
     """
     Generates helper files for serving plumber APIs
 
@@ -151,19 +160,22 @@ def generate_plumber(plumber_file, overwrite=False, verbose=False):
     plumber_file   -- path to your plumber file to serve
     overwrite      -- whether to overwrite existing helper files
     verbose        -- prints more verbose output
+    port           -- The port to listen on [default: 8888]
 
     Generated file:
-    * app.sh -- starts the plumber API with Domino specific parameters
+    * app.sh -- starts the plumber API
     """
     print("Generating app.sh...")
     if not overwrite:
         file_exists("app.sh", print_err=True)
     file_does_not_exist(plumber_file, print_err=True)
+    app_template = PLUMBER_APP_START.format(plumber_file, port)
     with open("app.sh", "w", encoding="utf8") as file:
-        file.write(PLUMBER_APP_START.format(plumber_file))
+        file.write(app_template)
+    chmod("app.sh", 0o755)
     if verbose:
         print("---- app.sh ----")
-        print(PLUMBER_APP_START.format(plumber_file))
+        print(app_template)
     print("Complete.")
 
 
@@ -174,23 +186,36 @@ def cli_arg_parser(args=None):
     """
     parser = argparse.ArgumentParser(
         prog="rgear",
-        description="Generates R helpers for Domino",
+        description="Generates helper scripts for starting R based content servers",
         epilog="For detailed help, please see https://github.com/sellorm/rgear",
     )
     parser.add_argument(
         "content_type",
-        help="Choose the application type you want to serve",
+        help="choose the application type you want to serve",
         choices=["shiny", "plumber", "rmarkdown"],
     )
     parser.add_argument("path", help="path to the content to serve")
     parser.add_argument(
         "-f",
         "--force",
-        help="Forces overwrite if the output file(s) already exist",
+        help="force overwrite if the output file(s) already exist",
         action="store_true",
     )
-    parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument('--version', action='version', version= __version__)
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="override the default port setting. [default: %(default)s]",
+        default=8888,
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="enable verbose output", action="store_true"
+    )
+    parser.add_argument(
+        "--version",
+        help="print the version and exit",
+        action="version",
+        version=__version__,
+    )
     return parser.parse_args(args)
 
 
@@ -201,13 +226,19 @@ def main():
     args = cli_arg_parser(sys.argv[1:])
 
     if args.content_type == "shiny":
-        generate_shiny(args.path, overwrite=args.force, verbose=args.verbose)
+        generate_shiny(
+            args.path, overwrite=args.force, verbose=args.verbose, port=args.port
+        )
 
     if args.content_type == "rmarkdown":
-        generate_rmarkdown(args.path, overwrite=args.force, verbose=args.verbose)
+        generate_rmarkdown(
+            args.path, overwrite=args.force, verbose=args.verbose, port=args.port
+        )
 
     if args.content_type == "plumber":
-        generate_plumber(args.path, overwrite=args.force, verbose=args.verbose)
+        generate_plumber(
+            args.path, overwrite=args.force, verbose=args.verbose, port=args.port
+        )
 
 
 if __name__ == "__main__":
